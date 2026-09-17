25cd2a5704b31544912eca3435dd1a7a2d12ea69
Brad Wilson <mrbrad8381@gmail.com>
2026-09-18 00:35:10 +1000

    Add route sorting modes

---
+++ SpireGPSCode/Features/RoutePlanner/RouteSortMode.cs
@@ -0,0 +1,13 @@
+namespace SpireGPS.Features.RoutePlanner;
+
+internal enum RouteSortMode
+{
+    Preferred,
+    RouteOrder,
+    Monsters,
+    Unknowns,
+    Elites,
+    Shops,
+    RestSites,
+    Treasures
+}
