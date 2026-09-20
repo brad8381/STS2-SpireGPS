4154a55216d1e992d3839901c7413039f62649cc
Brad Wilson <mrbrad8381@gmail.com>
2026-09-20 23:09:17 +1000

    Add buffering semantics to Ghost Plan messages

--- SpireGPSCode/Features/GhostTurnPlanner/GhostTurnPlanner.cs
+++ SpireGPSCode/Features/GhostTurnPlanner/GhostTurnPlanner.cs
@@ -441,6 +441,7 @@ internal static class GhostTurnPlannerService
 public sealed class GhostPlanMessage : INetMessage, IPacketSerializable
 {
     public bool ShouldBroadcast => true;
+    public bool ShouldBuffer => false;
     public NetTransferMode Mode => NetTransferMode.Reliable;
     public LogLevel LogLevel => LogLevel.VeryDebug;
 
